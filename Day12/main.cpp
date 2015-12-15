4c72365dfe59fc7854adc6128c3af38a7a0401cc
Piotr Szczepanski <piotr.szczepanski@avg.com>
2015-12-15 16:28:24 +0100

    Added part 2.

--- Day12/main.cpp
+++ Day12/main.cpp
@@ -14,21 +14,27 @@ using boost::property_tree::ptree;
 using boost::property_tree::read_json;
 using boost::lexical_cast;
 using boost::bad_lexical_cast;
-int sum = 0;
 
-void countElements(ptree pt)
+int countElements(const ptree& pt, const bool& filterRed = false)
 {
+	int local = 0;
 	for (const auto& v : pt)
 	{
-		countElements(v.second);
+		local += countElements(v.second, filterRed);
 		try
 		{
-			sum += lexical_cast<int>(v.second.get_value<std::string>(""));
+			local += lexical_cast<int>(v.second.get_value<std::string>(""));
 		}
 		catch (const bad_lexical_cast&)
 		{
+			if (filterRed)
+			{
+				if ((v.first != "") && (v.second.get_value<std::string>("") == "red"))
+					return 0;
+			}
 		}
 	}
+	return local;
 }
 
 size_t partOne()
@@ -36,13 +42,21 @@ size_t partOne()
 	std::ifstream infile("data_d12.txt");
 	std::string input;
 	ptree pt;
-
 	read_json(infile, pt);
-	countElements(pt);
-	return sum;
+	return countElements(pt);
+}
+
+size_t partTwo()
+{
+	std::ifstream infile("data_d12.txt");
+	std::string input;
+	ptree pt;
+	read_json(infile, pt);
+	return countElements(pt, true);
 }
 
 int main()
 {
-	std::cout << partOne();
+	std::cout << "Part One: " << partOne() << std::endl;
+	std::cout << "Part Two: " << partTwo() << std::endl;
 }
