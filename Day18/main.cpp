86a111c8c139779c790b7349f039ccd63f607b46
Piotr Szczepanski <piotr.szczepanski@avg.com>
2015-12-18 15:41:30 +0100

    Added sentinels to array.

--- Day18/main.cpp
+++ Day18/main.cpp
@@ -21,86 +21,29 @@ int isLightOn(char ch)
 }
 
 template<int S>
-int countNeighboursOn(Grid<S>& grid, int x, int y, bool partTwo = false)
+int neighboursOn(Grid<S>& grid, int x, int y)
 {
-	int lightsOn = 0;
-	if (x == 0) // top row
-	{
-		if (y == 0) // top left
-		{
-			if (partTwo)
-				return 3;
-			lightsOn = isLightOn(grid[0][1]) + isLightOn(grid[1][0]) + isLightOn(grid[1][1]);
-		}
-		else if (y == grid[0].size()-1) // top right
-		{
-			if (partTwo)
-				return 3;
-			lightsOn = isLightOn(grid[0][y - 1]) + isLightOn(grid[1][y]) + isLightOn(grid[1][y - 1]);
-		}
-		else // rest of top row
-		{
-			lightsOn = isLightOn(grid[0][y - 1]) + isLightOn(grid[0][y + 1]) + \
-				isLightOn(grid[1][y - 1]) + isLightOn(grid[1][y]) + isLightOn(grid[1][y + 1]);
-		}
-	}
-	else if (x == grid.size() - 1) // bottom row
-	{
-		if (y == 0)	// bottom left
-		{
-			if (partTwo)
-				return 3;
-			lightsOn = isLightOn(grid[x - 1][0]) + isLightOn(grid[x][1]) + isLightOn(grid[x - 1][1]);
-		}
-		else if (y == grid[x].size() - 1) // bottom right
-		{
-			if (partTwo)
-				return 3;
-			lightsOn = isLightOn(grid[x - 1][y]) + isLightOn(grid[x - 1][y - 1]) + isLightOn(grid[x][y - 1]);
-		}
-		else // rest of bottom row
-		{
-			lightsOn = isLightOn(grid[x][y - 1]) + isLightOn(grid[x][y + 1]) + \
-				isLightOn(grid[x - 1][y - 1]) + isLightOn(grid[x - 1][y]) + isLightOn(grid[x - 1][y + 1]);
-		}
-	}
-	else if (y == 0) // left column
-	{
-		lightsOn =	isLightOn(grid[x - 1][0]) + isLightOn(grid[x - 1][1]) + \
-					isLightOn(grid[x][1]) + \
-					isLightOn(grid[x + 1][0]) + isLightOn(grid[x + 1][1]);
-	}
-	else if (y == grid.size() -1) // right column
-	{
-		lightsOn =	isLightOn(grid[x - 1][y - 1]) + isLightOn(grid[x - 1][y]) + \
-					isLightOn(grid[x][y - 1]) + \
-					isLightOn(grid[x + 1][y - 1]) + isLightOn(grid[x + 1][y]);
-	}
-	else
-	{
-		lightsOn = isLightOn(grid[x - 1][y - 1]) + isLightOn(grid[x - 1][y]) + isLightOn(grid[x - 1][y + 1]) + \
-			isLightOn(grid[x][y - 1]) + isLightOn(grid[x][y + 1]) + \
-			isLightOn(grid[x + 1][y - 1]) + isLightOn(grid[x + 1][y]) + isLightOn(grid[x + 1][y + 1]);
-	}
-	return lightsOn;
+	return isLightOn(grid[x - 1][y - 1]) + isLightOn(grid[x - 1][y]) + isLightOn(grid[x - 1][y + 1]) + \
+		isLightOn(grid[x][y - 1]) + isLightOn(grid[x][y + 1]) + \
+		isLightOn(grid[x + 1][y - 1]) + isLightOn(grid[x + 1][y]) + isLightOn(grid[x + 1][y + 1]);
 }
 
 template<int S>
-Grid<S> animate(Grid<S>& grid, bool partTwo = false)
+Grid<S> animate(Grid<S>& grid)
 {
 	Grid<S> newGrid = grid;
-	for (int i = 0; i < grid.size(); i++)
+	for (int i = 1; i < S - 1; i++)
 	{
-		for (int j = 0; j < grid.size(); j++)
+		for (int j = 1; j < S - 1; j++)
 		{
 			if (isLightOn(grid[i][j]))
 			{
-				if (countNeighboursOn(grid, i, j, partTwo) != 2 && countNeighboursOn(grid, i, j, partTwo) != 3)
+				if (neighboursOn(grid, i, j) != 2 && neighboursOn(grid, i, j) != 3)
 					newGrid[i][j] = '.';
 			}
 			else
 			{
-				if (countNeighboursOn(grid, i, j, partTwo) == 3)
+				if (neighboursOn(grid, i, j) == 3)
 					newGrid[i][j] = '#';
 			}
 		}
@@ -111,10 +54,10 @@ Grid<S> animate(Grid<S>& grid, bool partTwo = false)
 template<int S>
 void lightCorners(Grid<S>& grid)
 {
-	grid[0][0] = '#';
-	grid[0][(int)grid[0].size() - 1] = '#';
-	grid[(int)grid.size() - 1][0] = '#';
-	grid[(int)grid.size() - 1][(int)grid[0].size() - 1] = '#';
+	grid[1][1] = '#';
+	grid[1][S - 2] = '#';
+	grid[S - 2][1] = '#';
+	grid[S - 2][S - 2] = '#';
 }
 
 template<int S>
@@ -135,7 +78,7 @@ int countLights(Grid<S>& grid)
 template<int S>
 int partOne(Grid<S> grid)
 {
-	for (int i = 0; i < grid.size(); i++)
+	for (int i = 0; i < 100; i++)
 	{
 		grid = animate(grid);
 	}
@@ -145,13 +88,26 @@ int partOne(Grid<S> grid)
 template<int S>
 int partTwo(Grid<S> grid)
 {
-	for (int i = 0; i < grid.size(); i++)
+	for (int i = 0; i < 100; i++)
 	{
 		lightCorners(grid);
-		grid = animate(grid, true);
+		grid = animate(grid);
 	}
+	lightCorners(grid);
 	return countLights(grid);
 	return 1;
+}
+
+template<int S>
+void setGrid(Grid<S>& grid, char val)
+{
+	for (auto& row : grid)
+	{
+		for (auto& element : row)
+		{
+			element = val;
+		}
+	}
 }
 
 template<int S>
@@ -160,11 +116,12 @@ Grid<S> readGrid()
 	std::ifstream infile("data_d18.txt");
 	std::string input;
 	Grid<S> grid;
-	int i = 0;
+	int i = 1;
 
+	setGrid<S>(grid, '.');
 	while (std::getline(infile, input))
 	{
-		int j = 0;
+		int j = 1;
 		for (const auto& ch : input)
 		{
 			if (i > S || j > S)
@@ -179,7 +136,7 @@ Grid<S> readGrid()
 int main()
 {
 	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-	Grid<100> grid = readGrid<100>();
+	Grid<102> grid = readGrid<102>();
 	std::cout << partOne(grid) << std::endl;
 	std::cout << partTwo(grid) << std::endl;
 	std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now() - t1);
